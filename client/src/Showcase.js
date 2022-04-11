cf7f97a578bb275c4f1d9ccc1a523938ba0902b1
Colt Skeen <colton.skeen@gmail.com>
2022-04-11 13:19:58 -0400

    More styling - Created recipe card containers
    Changes:
    - changed footer image
    - added containers for cards
    - center them!
    - inject overflow scroll feature

--- client/src/Showcase.js
+++ client/src/Showcase.js
@@ -5,11 +5,18 @@ const Showcase = (props) => {
     
     return(
         <div className="showcase">
-            <input className="search" type="text" placeholder="   What are you looking for?"/>
-            <button type="submit" className="search-button">
-            <i class="fa-solid fa-magnifying-glass"></i>
-            </button>
-            <div className="cards"></div>
+            <div className="showcase-search">
+                <input className="search" type="text" placeholder="   What are you looking for?"/>
+                <button type="submit" className="search-button">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </div>
+            <div className="showcase-card-container">
+                <div className="showcase-card"></div>
+                <div className="showcase-card"></div>
+                <div className="showcase-card"></div>
+                <div className="showcase-card"></div>
+            </div>
         </div>
     );
 }
